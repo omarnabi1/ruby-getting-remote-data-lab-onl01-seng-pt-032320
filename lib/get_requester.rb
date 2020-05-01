0c8731d7761e61b53e3c0286287d84a82addc74f
Omar Nabi <onabi20@gmail.com>
2020-05-01 04:04:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,11 +4,11 @@ class GetRequester
     @url = url 
   end
   
-  def get_response_body
-    uri = URI.parse(@url)
-    response = Net::HTTP.get_response(uri)
-    response.body 
-  end
+ def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
   
   def parse_json
   end 
